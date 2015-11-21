687e8b66931481aad83fb86e5f7345aae65394c7
Jugaadu <swap.math@gmail.com>
2015-11-21 23:57:38 +0530

    Created RandomForestRegressor

--- dhackLassoCV.py
+++ dhackLassoCV.py
@@ -31,25 +31,24 @@ features_test = test.drop('User_ID',1)
 le = LabelEncoder()
 print("assuming text variables are categorical & replacing them with numeric ids\n")
 for c in featureNames:
-   features_train[c] = features_train[np.isnan(features_train[c])] = -1
-   features_test[c] = features_test[np.isnan(features_test[c])] = -1
+   # features_train[c] = features_train[np.isnan(features_train[c])] = -1
+   # features_test[c] = features_test[np.isnan(features_test[c])] = -1
    if features_train[c].dtype.name == 'object':
       
       le.fit(np.append(features_train[c],features_test[c])) 
       features_train[c] = le.transform(features_train[c]).astype(int)
       features_test[c] = le.transform(features_test[c]).astype(int)
 
-clf = LassoCV(cv = 10)	  
-dhackLassoModel = clf.fit(features_train,labels_train)
-pred = dhackLassoModel .predict(features_test)
+features_train = features_train.fillna(0)
+features_test = features_test.fillna(0)
+dhackLassoModel = LassoCV(cv = 10).fit(features_train,labels_train)
+pred = dhackLassoModel.predict(features_test)
 
-print pred
-
-submission = pd.DataFrame({"User_ID" : ids, "Product_ID" : pid, "Purchase" : pred})
+submission = pd.DataFrame({"User_ID": ids, "Product_ID": pid, "Purchase": pred})
 #print out the value of alpha that minimizes the CV-error
 print("alpha Value that Minimizes CV Error ",dhackLassoModel.alpha_)
 print("Minimum MSE ", min(dhackLassoModel.mse_path_.mean(axis=-1)))
 
-submission.to_csv("submission.csv", index=False)
+submission.to_csv("submissionLassocv.csv", index=False)
 
 
--- dhackRF.py
+++ dhackRF.py
@@ -4,12 +4,13 @@
 D Hack Analyticsvidya 11/20/2015
 '''
 
-from sklearn import linear_model
-from sklearn.linear_model import LassoCV
+from sklearn import ensemble
+from sklearn.ensemble import RandomForestRegressor 
 from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plot
 import pandas as pd
 import numpy as np
+import math
 
 # Read train data
 
@@ -28,27 +29,32 @@ pid = test['Product_ID'].values
 features_test = test.drop('User_ID',1)
 
 
+
+
+
 le = LabelEncoder()
 print("assuming text variables are categorical & replacing them with numeric ids\n")
 for c in featureNames:
-   features_train[c] = features_train[np.isnan(features_train[c])] = -1
-   features_test[c] = features_test[np.isnan(features_test[c])] = -1
+   
    if features_train[c].dtype.name == 'object':
       
       le.fit(np.append(features_train[c],features_test[c])) 
       features_train[c] = le.transform(features_train[c]).astype(int)
       features_test[c] = le.transform(features_test[c]).astype(int)
 
-clf = LassoCV(cv = 10)	  
-dhackLassoModel = clf.fit(features_train,labels_train)
-pred = dhackLassoModel .predict(features_test)
-
-print pred
+features_train = features_train.fillna(0)
+features_test = features_test.fillna(0)
+print features_train
+print labels_train
+dhackModel = RandomForestRegressor(min_samples_split = 20)
+dhackModel.fit(features_train,labels_train)
+pred = dhackModel.predict(features_test)
+print features_test
 
 submission = pd.DataFrame({"User_ID" : ids, "Product_ID" : pid, "Purchase" : pred})
 #print out the value of alpha that minimizes the CV-error
-print("alpha Value that Minimizes CV Error ",dhackLassoModel.alpha_)
-print("Minimum MSE ", min(dhackLassoModel.mse_path_.mean(axis=-1)))
+#print("alpha Value that Minimizes CV Error ",dhackLassoModel.alpha_)
+#print("Minimum MSE ", min(dhackLassoModel.mse_path_.mean(axis=-1)))
 
 submission.to_csv("submission.csv", index=False)
 
